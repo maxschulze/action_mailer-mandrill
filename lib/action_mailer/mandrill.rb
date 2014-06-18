00762d71a82cb4c95dabf737f9e6b9c3bf7791eb
Max Schulze <max@maxschulze.com>
2014-06-18 14:37:45 +0200

    init

---
+++ lib/action_mailer/mandrill.rb
@@ -0,0 +1,7 @@
+require "action_mailer/mandrill/version"
+
+module ActionMailer
+  module Mandrill
+    # Your code goes here...
+  end
+end
---
+++ lib/action_mailer/mandrill/version.rb
@@ -0,0 +1,5 @@
+module ActionMailer
+  module Mandrill
+    VERSION = "0.0.1"
+  end
+end
